ab9e6e8dbd6d0c43507925c69c7e55296403f891
Alena Petrych <abelokonskaya.87@gmail.com>
2025-11-19 11:28:40 +0000

    feat: add clear-btn and add-btn

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,14 +3,31 @@ import "./App.css";
 import Button from "./components/UI/Button.jsx";
 
 function App() {
+  // handle button actions
+  const handleClear = () => {
+    console.log("Clear completed task");
+  };
+
+  const handleAdd = () => {
+    console.log("Add new task");
+  };
+
   return (
     <>
       <h1>My ToDos</h1>
       <p>tasks</p>
 
       <h2>Completed</h2>
+
       <p>tasks</p>
-      <Button />
+
+      <Button onClick={handleClear} className="clear-btn">
+        Clear
+      </Button>
+
+      <Button onClick={handleAdd} className="add-btn">
+        Add new task
+      </Button>
     </>
   );
 }
