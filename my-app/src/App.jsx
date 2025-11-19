5834915a165fbbefad18730fb42f90e578f12fec
Alena Petrych <abelokonskaya.87@gmail.com>
2025-11-19 11:11:26 +0000

    style: add style for button

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,8 +1,18 @@
 import { useState } from "react";
 import "./App.css";
+import Button from "./components/UI/Button.jsx";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>My ToDos</h1>
+      <p>tasks</p>
+
+      <h2>Completed</h2>
+      <p>tasks</p>
+      <Button />
+    </>
+  );
 }
 
 export default App;
