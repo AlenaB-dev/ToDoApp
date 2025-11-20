415cf2b9fc2895144d109c42512e606cf66ceb62
Alena Petrych <abelokonskaya.87@gmail.com>
2025-11-20 12:36:54 +0000

    feat: add TodoActions component

--- my-app/src/components/Todos/TodoActions.jsx
+++ my-app/src/components/Todos/TodoActions.jsx
@@ -1,6 +1,25 @@
+import { categories } from "../utils/categories";
 
+const TodoActions = ({ onSelect }) => {
+  return (
+    <div style={{ display: "flex", gap: "12px", padding: "10px" }}>
+      {categories.map((cat) => (
+        <button
+          key={cat.id}
+          onClick={() => onSelect(cat.id)}
+          style={{
+            border: "none",
+            background: "transparent",
+            fontSize: "24px",
+            cursor: "pointer",
+          }}
+          title={cat.label}
+        >
+          {cat.icon}
+        </button>
+      ))}
+    </div>
+  );
+};
 
-const TodoActions = () => {
-    return ()
-
-}
+export default TodoActions;
