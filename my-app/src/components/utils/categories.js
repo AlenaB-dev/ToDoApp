67cfcd46fe59ee10f8d915f2dfc9139499a570fe
Alena Petrych <abelokonskaya.87@gmail.com>
2025-11-20 12:31:40 +0000

    feat: add FaIcons and categories.js file

---
+++ my-app/src/components/utils/categories.js
@@ -0,0 +1,15 @@
+import {
+  FaHome,
+  FaAward,
+  FaAngellist,
+  FaAppleWhole,
+  FaBalanceScale,
+} from "react-icons/fa";
+
+export const categories = [
+  { id: "home", label: "Home", icon: <FaHome /> },
+  { id: "study", label: "Study", icon: <FaAward /> },
+  { id: "fun", label: "Fun", icon: <FaAngellist /> },
+  { id: "grocery", label: "Grocery", icon: <FaAppleWhole /> },
+  { id: "mindset", label: "Mindset", icon: <FaBalanceScale /> },
+];
